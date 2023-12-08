6ea7d6945cdb75c03a72c42cfbdbc93a1795de86
LiranBratt2121 <brattlirannin@gmail.com>
2023-12-08 23:31:21 +0200

    Add distance_tracking class

--- distance_tracking.py
+++ distance_tracking.py
@@ -1,8 +1,8 @@
 class ObjDistanceFinder:
-    def __init__(self, focal_length, real_width, width_in_frame):
+    def __init__(self, real_width_cm: float, width_in_frame_px: float, focal_length: float | None = None):
         self._focal_length = focal_length
-        self.real_width = real_width
-        self.width_in_frame = width_in_frame
+        self.real_width_cm = real_width_cm
+        self.width_in_frame_px = width_in_frame_px
 
     @property
     def focal_length(self):
@@ -12,8 +12,8 @@ class ObjDistanceFinder:
     def focal_length(self, calculated_focal_length):
         self._focal_length = calculated_focal_length
 
-    def calculate_focal_length(self, width_in_rf_image: float, measured_distance: float):
-        self.focal_length = (width_in_rf_image * measured_distance) / self.real_width
+    def calculate_focal_length(self, measured_distance_cm: float):
+        self.focal_length = (((self.width_in_frame_px * measured_distance_cm) / self.real_width_cm))
 
     def find_distance(self):
-        return (self.real_width * self.focal_length) / self.width_in_frame
+        return ((((self.real_width_cm * self.focal_length) / self.width_in_frame_px)) / 2)
